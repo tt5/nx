1d23c499e3c75807328499f32e54c211c4260971
n <2585803+tt5@users.noreply.github.com>
2022-07-25 15:44:01 +0200

    minify a bit

---
+++ apps/content/dist/index.js
@@ -0,0 +1,9 @@
+import { Hono } from "hono";
+import { html } from "hono/html";
+import { cors } from "hono/cors";
+const app = new Hono();
+app.use("*", cors());
+app.get("/", (c) => {
+  return c.html(html`<h1 x-data="{ message: 'content2' }" x-text="message" ></h1>`);
+});
+export default app;
