4eee46a00cc43b24d85c7ba100e59da6903fdae0
n <2585803+tt5@users.noreply.github.com>
2022-08-01 19:13:26 +0200

    flex page

--- apps/first/src/index.ts
+++ apps/first/src/index.ts
@@ -15,7 +15,7 @@ if (!indexCss) return c.notFound();
       <html lang="en">
         <head>
           <meta charset="UTF-8" />
-          <title>My Homepage</title>
+          <title>Tt5 Home</title>
           <link
             rel="shortcut icon"
             href="data:image/svg+xml,<><path d='M10 8.99a1 1 0 00-.695 1.717l4.004 4a1 1 0 101.414-1.414l-4.004-4A1 1 0 0010 8.99z' fill='%2380b0ff' stroke='%235D7FDDaa' stroke-width='.3'/><path d='M6.508 1C3.48 1 1.002 3.474 1.002 6.5S3.48 12 6.508 12s5.504-2.474 5.504-5.5S9.536 1 6.508 1zm0 2a3.486 3.486 0 013.504 3.5c0 1.944-1.556 3.5-3.504 3.5a3.488 3.488 0 01-3.506-3.5C3.002 4.556 4.56 3 6.508 3z' fill='%2380b0ff' stroke='%235D7FDDaa' stroke-width='.3'/></svg>"
@@ -26,8 +26,9 @@ if (!indexCss) return c.notFound();
             src="/services/alpinejs"
           ></script>
         </head>
-        <body class="cyan p-2">
+        <body class="cyan min-h-screen max-h-screen flex flex-col">
           <nav
+            class="h-[3rem] blue"
             x-data="{data: '...loading'}"
             x-init="data = await (await fetch('https://hello123.tk/services/nav')).text();
   "
@@ -35,12 +36,16 @@ if (!indexCss) return c.notFound();
             <span x-html="data"></span>
           </nav>
           <main
+            class="flex-grow overflow-scroll overflow-x-hidden"
             x-data="{data: '...loading'}"
             x-init="data = await (await fetch('https://hello123.tk/services/content')).text();
   "
           >
             <span x-html="data"></span>
           </main>
+          <footer
+            class="max-h-[3rem] blue"
+          >footer</footer>
         </body>
       </html> `
   );
