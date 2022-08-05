1d84811c90d3437be668c8797a1a9236b4a346ff
n <2585803+tt5@users.noreply.github.com>
2022-08-05 20:26:42 +0200

    button click color

--- apps/nav/src/index.ts
+++ apps/nav/src/index.ts
@@ -11,26 +11,71 @@ app.get('/', (c) => {
     html`
 <div class="min-v-screen max-v-screen flex">
   <div>
-    nav-left
+  <div
+    x-data="{open: true}"
+    style="position: relative"
+  >
+    <button
+      x-init="$watch('open', (a) => { setTimeout(() => { open = true; }, '150') })"
+      class="button yellow"
+      style="position: absolute; top: 0; left: 0;"
+    >
+      <strong>tt5</strong>
+    </button>
+    <button
+      style="position: absolute; top: 0; left: 0;"
+      x-show="open" x-transition
+      x-init="$watch('$store.contentCurrent', (a) => { open = true; })"
+      :class="
+        $store.contentCurrent=='home' ? 'button bg-white' : 'button cyan'
+      "
+      @click="
+        open = false;
+        if ($store.contentCurrent=='home') {}
+        else {
+          $store.contentCurrent = 'home';
+        }
+      "
+    >
+      <strong>tt5</strong>
+    </button>
+  </div>
   </div>
   <div class="flex-grow"></div>
   <div class="flex gap-2">
-    <button
-      :class="$store.contentCurrent=='chess' ? 'button bg-white' : 'button cyan'"
-      x-data="{datab: '<span></span>', children: {}, present: false}"
-      @click="
-      children = document.querySelector('#content').childNodes;
-      children.forEach(li => {
-        if (li.id == 'content-chess') {present = true};
-      });
-      if (!present) {
-        datab = await (await fetch('https://hello123.tk/services/content/chess')).text();
-      };
-      $store.contentCurrent = 'chess';
-      "
-    >Chess
-      <span x-html="datab"></span>
-    </button>
+    <div
+      x-data="{open: true}"
+      style="position: relative"
+      class="button"
+    >
+      <button
+        x-init="$watch('open', (a) => { setTimeout(() => { open = true; }, '150') })"
+        class="button yellow"
+        style="position: absolute; top: 0; left: 0;"
+      >
+        Chess
+      </button>
+      <button
+        style="position: absolute; top: 0; left: 0;"
+        x-show="open" x-transition
+        x-init="$watch('$store.contentCurrent', (a) => { open = true; })"
+        :class="$store.contentCurrent=='chess' ? 'button bg-white' : 'button cyan'"
+        x-data="{datab: '<span></span>', children: {}, present: false}"
+        @click="
+          open = false;
+          children = document.querySelector('#content').childNodes;
+          children.forEach(li => {
+            if (li.id == 'content-chess') {present = true};
+          });
+          if (!present) {
+            datab = await (await fetch('https://hello123.tk/services/content/chess')).text();
+          };
+          $store.contentCurrent = 'chess';
+        "
+      >Chess
+        <span x-html="datab"></span>
+      </button>
+    </div>
     <button
       :class="$store.contentCurrent=='math' ? 'button bg-white' : 'button cyan'"
       x-data="{datab: '<span></span>', children: {}, present: false}"
