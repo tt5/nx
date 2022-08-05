3dcd4cba611925665778652e53c318fa48545ed6
n <2585803+tt5@users.noreply.github.com>
2022-08-06 01:10:53 +0200

    nav style

--- apps/nav/src/index.ts
+++ apps/nav/src/index.ts
@@ -1,34 +1,39 @@
 import { Hono } from 'hono';
-import { html } from 'hono/html';
+import { html, raw } from 'hono/html';
 import { cors } from 'hono/cors';
 
 const app = new Hono();
 app.use('*', cors());
 
+const buttonClicked = (text) => html`
+<button
+  x-init="$watch('open', (a) => { setTimeout(() => { open = true; }, '150') })"
+  class="button yellow"
+  style="position: absolute; top: 0; left: 0;"
+>
+  ${raw(text)}
+</button>
+`
+
 app.get('/', (c) => {
   //c.header('Cache-Control', 'private, max-age=60')
   return c.html(
     html`
-<div class="min-v-screen max-v-screen flex">
+<div 
+  class="min-v-screen max-v-screen flex"
+  style="font-size: 1.125rem;"
+>
   <div>
   <div
     x-data="{open: true}"
     style="position: relative"
   >
-    <button
-      x-init="$watch('open', (a) => { setTimeout(() => { open = true; }, '150') })"
-      class="button yellow"
-      style="position: absolute; top: 0; left: 0;"
-    >
-      <strong>tt5</strong>
-    </button>
+    ${buttonClicked("<strong>tt5</strong>")}
     <button
       style="position: absolute; top: 0; left: 0;"
       x-show="open" x-transition
       x-init="$watch('$store.contentCurrent', (a) => { open = true; })"
-      :class="
-        $store.contentCurrent=='home' ? 'button bg-white' : 'button cyan'
-      "
+      :class="$store.contentCurrent=='home' ? 'button bg-white' : 'button cyan'"
       @click="
         open = false;
         if ($store.contentCurrent=='home') {}
@@ -48,13 +53,7 @@ app.get('/', (c) => {
       style="position: relative"
       class="button"
     >
-      <button
-        x-init="$watch('open', (a) => { setTimeout(() => { open = true; }, '150') })"
-        class="button yellow"
-        style="position: absolute; top: 0; left: 0;"
-      >
-        Chess
-      </button>
+      ${buttonClicked("Chess")}
       <button
         style="position: absolute; top: 0; left: 0;"
         x-show="open" x-transition
@@ -76,38 +75,60 @@ app.get('/', (c) => {
         <span x-html="datab"></span>
       </button>
     </div>
-    <button
-      :class="$store.contentCurrent=='math' ? 'button bg-white' : 'button cyan'"
-      x-data="{datab: '<span></span>', children: {}, present: false}"
-      @click="
-      children = document.querySelector('#content').childNodes;
-      children.forEach(li => {
-        if (li.id == 'content-math') {present = true};
-      });
-      if (!present) {
-        datab = await (await fetch('https://hello123.tk/services/content/math')).text();
-      };
-      $store.contentCurrent = 'math';
-      "
-    >Math
-      <span x-html="datab"></span>
-    </button>
-    <button
-      :class="$store.contentCurrent=='notes' ? 'button bg-white' : 'button cyan'"
-      x-data="{datab: '<span></span>', children: {}, present: false}"
-      @click="
-      children = document.querySelector('#content').childNodes;
-      children.forEach(li => {
-        if (li.id == 'content-notes') {present = true};
-      });
-      if (!present) {
-        datab = await (await fetch('https://hello123.tk/services/content/notes')).text();
-      };
-      $store.contentCurrent = 'notes';
-      "
-      >Notes
-      <span x-html="datab"></span>
-    </button>
+    <div
+      x-data="{open: true}"
+      style="position: relative"
+      class="button"
+    >
+      ${buttonClicked("Math")}
+      <button
+        style="position: absolute; top: 0; left: 0;"
+        x-show="open" x-transition
+        x-init="$watch('$store.contentCurrent', (a) => { open = true; })"
+        :class="$store.contentCurrent=='math' ? 'button bg-white' : 'button cyan'"
+        x-data="{datab: '<span></span>', children: {}, present: false}"
+        @click="
+          open = false;
+          children = document.querySelector('#content').childNodes;
+          children.forEach(li => {
+            if (li.id == 'content-math') {present = true};
+          });
+          if (!present) {
+            datab = await (await fetch('https://hello123.tk/services/content/math')).text();
+          };
+          $store.contentCurrent = 'math';
+        "
+      >Math
+        <span x-html="datab"></span>
+      </button>
+    </div>
+    <div
+      x-data="{open: true}"
+      style="position: relative"
+      class="button"
+    >
+      ${buttonClicked("Notes")}
+      <button
+        style="position: absolute; top: 0; left: 0;"
+        x-show="open" x-transition
+        x-init="$watch('$store.contentCurrent', (a) => { open = true; })"
+        :class="$store.contentCurrent=='notes' ? 'button bg-white' : 'button cyan'"
+        x-data="{datab: '<span></span>', children: {}, present: false}"
+        @click="
+          open = false;
+          children = document.querySelector('#content').childNodes;
+          children.forEach(li => {
+            if (li.id == 'content-notes') {present = true};
+          });
+          if (!present) {
+            datab = await (await fetch('https://hello123.tk/services/content/notes')).text();
+          };
+          $store.contentCurrent = 'notes';
+        "
+        >Notes
+        <span x-html="datab"></span>
+      </button>
+    </div>
   </div>
 </div>
     `
